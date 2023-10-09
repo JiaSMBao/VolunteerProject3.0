da3aa9fffd013e9adb6510e15fe5533fb9e94d97
Jia <michelj9012@gmail.com>
2023-10-09 18:12:06 -0400

    Initial Commit

---
+++ VolunteerProject3.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VolunteerProject3.0
+//
+//  Created by Jia Michel on 10/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VolunteerProject3.0/VolunteerProject3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  VolunteerProject3_0App.swift
+//  VolunteerProject3.0
+//
+//  Created by Jia Michel on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct VolunteerProject3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
